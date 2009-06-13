0cef2d90e3db6723ed4a916c60b95e5968cac588
Henrik N <henrik@nyh.se>
2009-06-13 22:04:15 +0200

    Organize JS better, and add JS to spell out glyphs in the title="" of the keyboard shortcut <code> element.

--- js/app.js
+++ js/app.js
@@ -1,32 +1,87 @@
-$(function() {
+var Blog = {
   
-  $("#tweets").getTwitter({
-    userName: "henrik",
-    numTweets: 3,
-    slideIn: true,
-    showHeading: true,
-    headingText: 'Recently on <a href="http://twitter.com/henrik">Twitter</a>:',
-    showProfileLink: false,
-    rejectRepliesOutOf: 20
-  });
+  getTwitter: function() {
+    $("#tweets").getTwitter({
+      userName: "henrik",
+      numTweets: 3,
+      slideIn: true,
+      showHeading: true,
+      headingText: 'Recently on <a href="http://twitter.com/henrik">Twitter</a>:',
+      showProfileLink: false,
+      rejectRepliesOutOf: 20
+    });
+  },
+  
 
-  if ($.cookie('editable') && $('#edit').length) {
-    $('#edit a').text('Edit').before('[').after(']');
+  addEditLinks: function() {
+    if ($.cookie('editable') && $('#edit').length) {
+      $('#edit a').text('Edit').before('[').after(']');
+    }
+  },
+  
+
+  relativizeAge: function() {
+    $('#birthday').each(function() {
+        var text  = $(this).text();
+        var born  = new Date(text);
+        var today = new Date();
+        var ydiff = today.getFullYear() - born.getFullYear();
+        var bday = new Date(text);
+        bday.setFullYear(today.getFullYear());
+        if (bday > today) ydiff -= 1;
+
+        $(this).append(' ('+ydiff+' years old)');
+    });
+  },
+  
+
+  explainGlyphs: function() {
+    var glyphs = {
+      "␣": "Space",
+      "↩": "Return",
+      "⌅": "Enter",
+      "⇥": "Tab",
+      "⇤": "Backtab",
+      "⌫": "Backspace",
+      "⌦": "Forward Delete",
+      "⌘": "Command",
+      "⌥": "Option",
+      "^": "Control",
+      "⌃": "Control",
+      "⇧": "Shift",
+      "⇪": "Caps Lock",
+      "←": "Left Arrow",
+      "→": "Right Arrow",
+      "↑": "Up Arrow",
+      "↓": "Down Arrow",
+      "↖": "Home",
+      "↘": "End",
+      "⇞": "Page Up",
+      "⇟": "Page Down",
+      "⎋": "Escape",
+      "⏏": "Eject"
+    }
+
+    function glyphsToNames(text) {
+      return text.split('').map(function(x) { return (glyphs[x] || x) }).join(" ");
+    }
+    $('.kb').each(function() {
+      this.title = "Keys: " + glyphsToNames($(this).text());
+    });
+  },
+  
+
+  init: function() {
+    this.getTwitter();
+    this.explainGlyphs();
+    this.addEditLinks();
+    this.relativizeAge();
   }
   
-  $('#birthday').each(function() {
-      var text  = $(this).text();
-      var born  = new Date(text);
-      var today = new Date();
-      var ydiff = today.getFullYear() - born.getFullYear();
-      var bday = new Date(text);
-      bday.setFullYear(today.getFullYear());
-      if (bday > today) ydiff -= 1;
+};
 
-      $(this).append(' ('+ydiff+' years old)');
-  });
-  
-});
+$(function() { Blog.init() });
+
 
 // To show edit links:
 //   javascript: edit(true)
