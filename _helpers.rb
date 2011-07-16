7dda0c3c2cf7e5cd088cd2826ff829e1cf27cef3
Henrik N <henrik@nyh.se>
2011-07-16 11:10:56 +0200

    HTML5-ify, strip some whitespace.

--- _helpers.rb
+++ _helpers.rb
@@ -30,32 +30,32 @@ module Helpers
       "#{array[0...-1].join(', ')} #{connector} #{array[-1]}"
     end
   end
-  
+
   def header(text)
     "<h1>%s</h1>" % link_to(text, '/')
   end
-  
+
   def post_link(post)
     link_to(h(post.title), post.url)
   end
-  
+
   def tag_link(name, link_name=nil)
     link_name ||= name
     link_to(h(name), "/tag##{slug(link_name)}")
   end
-  
+
   def tag_links(array)
     links = array.map { |tag| tag_link(tag) }
     array_to_sentence(links)
   end
-  
+
   def slug(text)
     Slugalizer.slugalize(text)
   end
 
   def tag_cloud(tags, from=1, unto=6)
     return @@tag_cloud if defined?(@@tag_cloud)
-    
+
     tag_counts = tags.map {|tag,posts| [tag, posts.length] }.sort_by {|tag, count| tag.downcase }
     min = tag_counts.min { |a,b| a.last <=> b.last }.last
     max = tag_counts.max { |a,b| a.last <=> b.last }.last
@@ -65,14 +65,14 @@ module Helpers
       size = from + ((unto-from)*weight).round
       [tag, count, size]
     }
-    
+
     @@tag_cloud = ['<ol id="tag-cloud">',
         tag_counts_sizes.map {|t,c,s|
           title = c == 1 ? "1 post" : "#{c} posts"
           %{<li class="tier-#{s}" title="#{title}">#{tag_link(t)}</li>}
         }.join(' '),
       '</ol>'
-    ].join  
+    ].join
   end
 
 end
